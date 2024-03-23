a3f090c6d305a972add08294cb73fca19bdaeaac
Pia Valentin <9430235+piavalentin@users.noreply.github.com>
2024-03-24 00:37:30 +0100

    fix: pylint for test files

--- tests/test_find_translations.py
+++ tests/test_find_translations.py
@@ -1,9 +1,15 @@
+"""
+Testing translation lookup
+"""
 import unittest
 from unittest.mock import patch
 from tolino_notes_transformator.translations import find_translations
 
+
+# pylint: disable=missing-class-docstring
 class TestFindTranslations(unittest.TestCase):
-    @patch('googletrans.Translator.translate')
+    @patch("googletrans.Translator.translate")
+    # pylint: disable=missing-function-docstring
     def test_translation(self, mock_translate):
-        mock_translate.return_value.text = 'World'
-        self.assertEqual(find_translations(['welt']), {'world'})
+        mock_translate.return_value.text = "World"
+        self.assertEqual(find_translations(["welt"]), {"world"})
